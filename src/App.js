3bf542e0c2fd19d772fd3a88b44580e66aab785e
Luis Zubia <soyzubia@gmail.com>
2023-02-22 00:40:49 -0600

    Add Routes for root, /name, /name/:name and /* for no path error

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import NoClue from './pages/NoClue';
+import Home from './pages/Home';
+import Nations from './pages/Nations';
+import Nation from './pages/Nation';
 import './App.css';
 
 const App = () => (
   <div className="App">
-    <h1>NationClues WebApp</h1>
+    <BrowserRouter>
+      <h1>NationClues NAV</h1>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="nations" element={<Nations />} />
+        <Route path="nations/:name" element={<Nation />} />
+        <Route path="*" element={<NoClue />} />
+      </Routes>
+    </BrowserRouter>
   </div>
 );
 
