087c640ec362b504de4ea11994b12005b98a910c
Luis Zubia <soyzubia@gmail.com>
2023-02-22 23:51:55 -0600

    Add Routes for root, /nations, /nations/:name, /regions, /regions/:name and, /* for render a not found page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './components/Header/Header';
 import NoClue from './pages/NoClue';
 import Home from './pages/Home';
 import Nations from './pages/Nations';
 import Nation from './pages/Nation';
+import Regions from './pages/Regions';
+import Region from './pages/Region';
 import './App.css';
-import NavBar from './components/NavBar/NavBar';
 
 const App = () => (
-  <div className="App">
-    <BrowserRouter>
-      <NavBar />
-      <h1>NationClues NAV</h1>
+  <BrowserRouter>
+    <Header />
+    <main className="App">
       <Routes>
         <Route index element={<Home />} />
-        <Route path="nations" element={<Nations />} />
-        <Route path="nations/:name" element={<Nation />} />
+        <Route caseSensitive path="nations" element={<Nations />} />
+        <Route caseSensitive path="nations/:name" element={<Nation />} />
+        <Route caseSensitive path="region" element={<Regions />} />
+        <Route caseSensitive path="region/:name" element={<Region />} />
         <Route path="*" element={<NoClue />} />
       </Routes>
-    </BrowserRouter>
-  </div>
+    </main>
+  </BrowserRouter>
 );
 
 export default App;
